888a7b848e93476ac8f47a6844517047b48af9bb
arbaaz833 <arbaazali031@gmail.com>
2023-12-08 19:04:55 +0500

    link redirect

--- src/requestAccess.jsx
+++ src/requestAccess.jsx
@@ -1,13 +1,17 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import liveStreamService from "./services";
+import { CLIENT_ID, REDIRECT_URI } from "./config/config";
 
 function App() {
-  const reqAccess = useCallback(async () => {
-    await liveStreamService.requestAccess();
-  });
+  const linkRef = useRef();
+  const reqAccess = () => {
+    // await liveStreamService.requestAccess();
+    console.log("SSSSSSSSSS");
+    linkRef.current.click();
+  };
 
   return (
     <>
@@ -21,6 +25,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <a
+          href={`https://accounts.google.com/o/oauth2/v2/auth?
+        scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly&
+        state=state_parameter_passthrough_value&
+        redirect_uri=${REDIRECT_URI}&
+        response_type=token&
+        client_id=${CLIENT_ID}`}
+          target="_blank"
+          style={{ display: "none" }}
+          ref={linkRef}
+        ></a>
         <button onClick={reqAccess}>Grant Access</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
