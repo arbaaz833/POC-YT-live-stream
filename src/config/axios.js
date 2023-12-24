f422ef517fd3156cc5d9ecfa49f20bd499cb4113
arbaaz833 <arbaazali031@gmail.com>
2023-12-24 17:04:11 +0500

    token fix

--- src/config/axios.js
+++ src/config/axios.js
@@ -8,7 +8,7 @@ instance.interceptors.request.use(
   function (config) {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = token;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
