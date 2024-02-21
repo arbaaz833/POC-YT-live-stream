4734209cca6986f97b5ca756ed2dc491d274b84b
arbaaz833 <arbaazali031@gmail.com>
2024-02-22 00:02:12 +0500

    link update

--- src/root.jsx
+++ src/root.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Stream from "./pages/stream/stream.jsx";
 import ReqAccess from "./requestAccess.jsx";
 import { ToastContainer } from "react-toastify";
-import StreamNative from "./pages/stream/streamNative.jsx";
+import Stream from "./pages/stream/stream.jsx";
 
 const router = createBrowserRouter([
   {
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
   },
   {
     path: "stream",
-    element: <StreamNative />,
+    element: <Stream />,
   },
 ]);
 
