7f5a5c09715c23be2f2e73d260da1811c4481ab0
arbaaz833 <arbaazali031@gmail.com>
2024-03-03 03:58:16 +0500

    cue point check

--- src/pages/stream/stream.jsx
+++ src/pages/stream/stream.jsx
@@ -77,7 +77,7 @@ export default function Stream() {
       const data = {
         cueType: "cueTypeAd",
         durationSecs: 120,
-        insertionOffsetTimeMs: 0,
+        insertionOffsetTimeMs: 10000,
       };
       await liveStreamService.addCuepoint(eventId, data);
       notify.success("break added!");
