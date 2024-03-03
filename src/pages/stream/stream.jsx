51a876f729b8de5e22afa0e7e6c079d339bb4607
arbaaz833 <arbaazali031@gmail.com>
2024-03-03 16:30:04 +0500

    stream latency fix

--- src/pages/stream/stream.jsx
+++ src/pages/stream/stream.jsx
@@ -103,7 +103,7 @@ export default function Stream() {
           monitorStream: {
             enableMonitorStream: false,
           },
-          enableLowLatency: true,
+          latencyPreference: "ultraLow",
         },
       };
 
